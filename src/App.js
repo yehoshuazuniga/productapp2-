4d2386e2a3a5408e74a07f89636336763a78816e
yehoshua <studyehoshua@gmail.com>
2022-10-27 20:03:56 +0200

    trabajar con redux y react-redux, trabajo hecho con javier y apuntes tema 11

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import ProductsAndSuppliers from "./ProductsAndSuppliers";
+import { Provider } from "react-redux";
+import dataStore from "./store";
 
-export default class App extends Component { 
+export default class App extends Component {
+
     render() {
-        return <ProductsAndSuppliers />
+        return (
+            <Provider store={dataStore}>
+                <ProductsAndSuppliers />
+            </Provider>
+        )
     }
 }
---
+++ src/store/modelActionTypes.js
@@ -0,0 +1,6 @@
+export const STORE = "STORE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
+
+// los action son objetos , tien un action type y payload que proporciona el dato
+// los action se crean con los action criators
